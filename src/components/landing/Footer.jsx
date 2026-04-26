038c30bcb4ec33fc466defe97073a176e605833b
Charith Haputhanthree <charith@infineit.com>
2026-04-26 11:09:58 +0000

    File changes

--- src/components/landing/Footer.jsx
+++ src/components/landing/Footer.jsx
@@ -49,7 +49,7 @@ export default function Footer() {
               <a href="https://www.workroo.com.au" target="_blank" rel="noopener noreferrer" title="Website" className="text-gray-400 hover:text-white transition-colors">
                 <Globe className="w-5 h-5" />
               </a>
-              <a href="https://www.facebook.com/workroo" target="_blank" rel="noopener noreferrer" title="Facebook" className="text-gray-400 hover:text-white transition-colors">
+              <a href="https://www.facebook.com/workroo/" target="_blank" rel="noopener noreferrer" title="Facebook" className="text-gray-400 hover:text-white transition-colors">
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24">
                   <path d="M22 12c0-5.522-4.477-10-10-10S2 6.478 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987H7.898V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"/>
                 </svg>
